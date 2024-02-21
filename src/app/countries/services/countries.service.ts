8196354644a2ca851857a377bf379a0d930536e9
Alonso <marenas@itsmarts.com.mx>
2024-02-21 10:02:44 -0600

    Buscar Por País y Región Funcional

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, withFetch } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -20,7 +20,31 @@ export class CountriesService {
   searchCapital( term: string ): Observable<Country[]>{
 
     const url = `${this.apiUrl}/capital/${term}`
-    return this.http.get<Country[]>(url);
 
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError( error => of([]) )
+
+      );
+  }
+  searchCountry( term: string ): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/name/${term}`
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError( error => of([]) )
+
+      );
+  }
+  searchRegion( region: string ): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/region/${region}`
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError( error => of([]) )
+
+      );
   }
 }
